0da0abc32d79ba3c3a56a51dc969633b28ac15ea
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-12-09 16:48:29 +0700

    Add files via upload

---
+++ Program_03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> book = new Queue<string>();
+        bool checks = true;
+        string name;
+        while(checks == true)
+        {         
+            name = Console.ReadLine();
+            for (int i = 0; i < book.GetLength();i++)
+            {
+                if (name == book.Get(i))
+                {
+                    Console.WriteLine("Name Same");
+                    name = Console.ReadLine();
+                    
+                }
+            }
+            if(name == "Exit")
+            {
+                checks = false;
+            }
+            else 
+            {
+                if(book.GetLength() > 2)
+                {
+                    book.Pop();
+                }
+                book.Push(name);
+            }
+        }
+        for (int i = 0; i < 3;i++)
+        {
+            Console.WriteLine(book.Get(i));
+        }
+    }
+}
