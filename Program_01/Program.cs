38730162c70dbc1b7c87cbc2087a8be8380b40dd
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-12-09 16:46:10 +0700

    Ex01

---
+++ Program_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CircularLinkedList<float> number = new CircularLinkedList<float>();
+        LinkedList<float> Finalnumber = new LinkedList<float>();
+        bool newnum = true;
+        float num,n,a;
+        while(newnum == true)
+        {
+            num = float.Parse(Console.ReadLine());
+            if(num > 0)
+            {
+                number.Add(num);
+            }
+            else
+            {
+                newnum = false;
+            }
+        }
+        n = float.Parse(Console.ReadLine());
+        a = (n-1)/2;
+        float total1 = 0;
+        float total2 = 0;
+        for(int i = 0; i < number.GetLength();i++)
+        {            
+            for(int j = 1; j <= a ;j++)
+            {
+                total1 += number.Get(i+j);
+            }
+            for(int j = 1; j <= a ;j++)
+            {
+                total2 += number.Get(i-j);
+            }
+            Finalnumber.Add((total1+total2+number.Get(i))/n);
+            total1 = 0;
+            total2 = 0;
+        }
+        for(int j = 0; j < Finalnumber.GetLength();j++)
+        {
+            Console.WriteLine(Finalnumber.Get(j));
+        }
+    }
+}
