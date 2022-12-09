39e243e0cac5918a385c76284eddccced9cdb628
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-12-09 16:59:06 +0700

    Ex04

---
+++ Program_04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tree<string> nameRepair = new Tree<string>();
+        string name = Console.ReadLine();
+        int money = int.Parse(Console.ReadLine());
+        string check = Console.ReadLine();
+        nameRepair.AddChild(-1,name);
+        int price = 0,total = 0;
+        while(total != money)
+        {
+            name = Console.ReadLine();
+            price = int.Parse(Console.ReadLine());
+            check = Console.ReadLine();
+            total += price;
+        }
+    
+    }
+}
