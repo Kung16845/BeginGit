bfb03e0aa39e427ffce16b7d489aa6f0d55e7314
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-12-09 16:47:22 +0700

    Ez02

---
+++ Program_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<string> A = new Stack<string>();
+        bool c = true;
+        int a = 0,b = 0;
+        string n;
+        while (c == true)
+        {
+            n = Console.ReadLine();
+            if(n == "(" || n == ")")
+            {
+                A.Push(n); 
+            }
+            else 
+            {
+                c = false;
+            }
+        }
+
+        for(int i = 0;i < A.GetLength();i++)
+        {
+            if (A.Get(i) == "(" )
+            {
+                a++;
+            }
+            if (A.Get(i) == ")")
+            {
+                b++;
+            }
+        }
+
+        if (a == b)
+        {
+            Console.WriteLine("Valid");
+        }
+        else 
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+}
