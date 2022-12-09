009a59c40a76a873bf7c1bb3e26d62964db7b21f
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-12-09 16:49:39 +0700

    Ez05

---
+++ Program_05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       BinarySearchTree<int> number = new BinarySearchTree<int>();
+       bool newnum = true;
+       int num,a = 0,b = 0;
+        while(newnum == true)
+        {
+            num = int.Parse(Console.ReadLine());
+            if(num > 0)
+            {
+                number.Add(num);
+            }
+            else
+            {
+                newnum = false;
+            }
+        }
+        int num1 =int.Parse(Console.ReadLine());
+        int num2 =int.Parse(Console.ReadLine());
+        for(int i = 0;i < number.GetLength();i++)
+        {
+            if(num1 == number.Get(i))
+            {
+                a = i;
+            }
+            if(num2 == number.Get(i))
+            {
+                b = i;
+            }
+        }
+        Console.WriteLine(Math.Abs(a-b+1));
+    }
+}
